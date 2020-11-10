4d42c273b05c7e9290b88a5a25dbbedaa807f0a7
JaanWilli <jan.willi@uzh.ch>
2020-11-10 15:37:54 +0100

    added ex 4.5

---
+++ ex4_5/between.h
@@ -0,0 +1,33 @@
+#ifndef EX4_5_BETWEEN_H
+#define EX4_5_BETWEEN_H
+
+#include <iostream>
+
+using namespace std;
+
+template<typename Iter>
+bool betweenImpl(Iter fst, Iter lst, Iter i, std::random_access_iterator_tag) {
+    cout << "(RA):" << endl;
+    return fst <= i && i <= lst;
+}
+
+template<typename Iter>
+bool betweenImpl(Iter fst, Iter lst, Iter i, std::forward_iterator_tag) {
+    cout << "(F)" << endl;
+    while (fst != i) {
+        if (fst == lst) {
+            return false;
+        }
+        ++fst;
+    }
+    return true;
+}
+
+template<typename Iter>
+static bool between(Iter fst, Iter lst, Iter i) {
+    typedef typename std::iterator_traits<Iter>::iterator_category category;
+    return betweenImpl(fst, lst, i, category());
+}
+
+
+#endif //EX4_5_BETWEEN_H
---
+++ ex4_5/main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <set>
+#include "between.h"
+
+using namespace std;
+
+template<typename Iter>
+int get_index(Iter fst, Iter cur) {
+    return std::distance(fst, cur);
+}
+
+template<typename Iter>
+void check_between(Iter begin, Iter fst, Iter lst, Iter i) {
+    if (between(fst, lst, i)) {
+        cout << "Index " << get_index(begin, i) << " is between " <<
+             get_index(begin, fst) << " and " << get_index(begin, lst) << endl;
+    }
+    else {
+        cout << "Index " << get_index(begin, i) << " is not between " <<
+             get_index(begin, fst) << " and " << get_index(begin, lst) << endl;
+    }
+}
+
+int main() {
+    list<int> l = {1, 2, 3, 4, 5, 6, 7, 8};
+
+    auto l_fst = l.begin();
+    auto l_lst = next(l_fst, 4);
+    auto l_i = next(l_fst, 2);
+
+    cout << "List test (positive) ";
+    check_between(l.begin(), l_fst, l_lst, l_i);
+
+    cout << "---------" << endl;
+
+    advance(l_i, 3);
+
+    cout << "List test (negative) ";
+    check_between(l.begin(), l_fst, l_lst, l_i);
+
+    cout << "---------" << endl;
+
+    vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8};
+    auto v_fst = v.begin();
+    auto v_lst = next(v_fst, 4);
+    auto v_i = next(v_fst, 2);
+
+    cout << "Vector test (positive) ";
+    check_between(v.begin(), v_fst, v_lst, v_i);
+
+    cout << "---------" << endl;
+
+    advance(v_i, 3);
+
+    cout << "Vector test (negative) ";
+    check_between(v.begin(), v_fst, v_lst, v_i);
+
+    cout << "---------" << endl;
+
+    set<int> s = {1, 2, 3, 4, 5, 6, 7, 8};
+    auto s_fst = s.begin();
+    auto s_lst = next(s_fst, 4);
+    auto s_i = next(s_fst, 2);
+
+    cout << "Set test (positive) ";
+    check_between(s.begin(), s_fst, s_lst, s_i);
+
+    cout << "---------" << endl;
+
+    advance(s_i, 3);
+
+    cout << "Set test (negative) ";
+    check_between(s.begin(), s_fst, s_lst, s_i);
+}
