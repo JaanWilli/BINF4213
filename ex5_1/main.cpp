433dc4bfa1b59301d80dc6ce84f346c5307c13dd
JaanWilli <jan.willi@uzh.ch>
2020-11-14 17:00:03 +0100

    first draft of ex5.1 and 5.4

--- ex5_1/main.cpp
+++ ex5_1/main.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
+#include <regex>
+#include "../ex3_2/pvector.h"
+#include "../ex3_2/pset.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+using namespace std;
+
+void start_check(string &d, string &f) {
+
+    pset<string> dict(d);
+    pvector<string> file(f);
+
+    for (string line : file) {
+        regex match_words("\\w+");
+
+        auto start = sregex_iterator(line.begin(), line.end(), match_words);
+        auto stop = sregex_iterator();
+
+        for(sregex_iterator w = start; w != stop; ++w) {
+            smatch match = *w;
+            string word = match.str();
+
+            for (const string& el : dict) {
+                regex match_dict(el);
+                if (regex_match(word, match_dict)) {
+                    cout << "Found a match: " << word << endl;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+
+    if (argc == 3) {
+        string dict(argv[1]);
+        string file(argv[2]);
+
+        start_check(dict, file);
+    }
 }
--- ex5_4/main.cpp
+++ ex5_4/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 
+using namespace std;
+
+class parent {
+public:
+    parent() {
+        cout << "Construct parent class" << endl;
+    }
+
+    ~parent() {
+        cout << "Destruct parent class" << endl;
+    }
+};
+
+class child : public parent {
+public:
+    child() {
+        cout << "Construct child class" << endl;
+    }
+
+    ~child() {
+        cout << "Destruct child class" << endl;
+    }
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    child c;
     return 0;
 }
