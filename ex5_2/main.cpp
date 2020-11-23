29dafcc4db1ddb7cc2ebb4809317ed8f4ba870c3
JaanWilli <jan.willi@uzh.ch>
2020-11-23 08:37:34 +0100

    modify ex5.2

--- ex5_2/main.cpp
+++ ex5_2/main.cpp
@@ -1,22 +1,15 @@
 #include <iostream>
 #include "jan_playfield.h"
 #include "game.h"
-#include "player.h"
 #include "human_player.h"
 #include "jan_player.h"
-
-#include <memory>
 
 using namespace std;
 
 typedef playfield_traits<playfield> PT;
 
-int main() {
-
-    auto p1 = human_player(1);
-    auto p2 = jan_player(2);
-    auto field = jan_playfield();
-
+template<typename P1, typename P2>
+void play(jan_playfield &field, P1 p1, P2 p2) {
     int current = 1;
 
     while (PT::is_not_full(field)) {
@@ -37,10 +30,16 @@ int main() {
         if (PT::has_won(field, current)) {
             field.print();
             std::cout << "Player " << current << " won!" << std::endl;
-            return 0;
+            return;
         }
 
         current = PT::next_player(current);
     }
     std::cout << "It's a draw..." << std::endl;
+}
+
+int main() {
+
+    auto field = jan_playfield();
+    play(field, human_player(1), jan_player(2));
 }
