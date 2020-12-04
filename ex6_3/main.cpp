8dfaf8699ec19dd498d054b12feed9aa28dee9a8
JaanWilli <jan.willi@uzh.ch>
2020-12-04 17:55:08 +0100

    completed ex 6

--- ex6_3/main.cpp
+++ ex6_3/main.cpp
@@ -17,19 +17,24 @@ struct aspolar {
     explicit aspolar(const complex<double> c, Flag flag) : c(c), f(flag) {}
 };
 
-inline ostream &operator<<(ostream &os, aspolar a) {
+ostream &operator<<(ostream &os, aspolar a) {
 
-    double rad = atan((a.c.imag() / a.c.real())) + ((a.c.real() < 0) ? M_PI : 0);
+    double radius = abs(a.c);
+    double theta = atan((a.c.imag() / a.c.real()));
+
+    if (a.c.real() < 0) theta += M_PI;
+    if (theta < 0) theta = 2 * M_PI + theta;
 
     if (a.f == a.DEG)
-        os << "Degrees: " << (rad * 180 / M_PI);
+        os << "Radius = " << radius << ", Theta = " << (theta * 180 / M_PI) << " degrees";
     else
-        os << "Radians: " << rad;
+        os << "Radius = " << radius << ", Theta = " << theta << " radians";
 }
 
 int main() {
     complex<double> c(3,4);
     cout << "Complex: " << c.real() << (c.imag() >= 0 ? "+" : "") << c.imag() << "i" << endl;
+
     cout << aspolar(c, aspolar::DEG) << endl;
     cout << aspolar(c) << endl;
 }
